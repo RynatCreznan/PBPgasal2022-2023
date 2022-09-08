7b4da91e4abcf34f55f81717b1836053c9c77714
Elang Permana <elang.permana@ui.ac.id>
2022-09-08 15:26:43 +0700

    revlab01

--- wishlist/views.py
+++ wishlist/views.py
@@ -3,10 +3,11 @@ from wishlist.models import BarangWishlist
 
 # Create your views here.
 def show_wishlist(request):
-    return render(request, "wishlist.html")
+   
+    data_barang_wishlist = BarangWishlist.objects.all()
+    context = {
+        'list_barang': data_barang_wishlist,
+        'nama': 'Elang Permana'
+    }
     return render(request, "wishlist.html", context)
-data_barang_wishlist = BarangWishlist.objects.all()
-context = {
-    'list_barang': data_barang_wishlist,
-    'nama': 'Elang Permana'
-}
+
